bc1d33e9610d877d180499e33848884d0505fa8b
Kathleen Garrity <garrityk@bc.edu>
2020-04-25 12:55:14 -0400

    Initial Commit

---
+++ TravelWise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelWise
+//
+//  Created by Kathleen Garrity on 4/25/20.
+//  Copyright © 2020 Kathleen Garrity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
