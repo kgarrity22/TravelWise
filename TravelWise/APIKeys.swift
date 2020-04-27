d499faeaee329abda57c75ef4f11c075d6c8031a
Kathleen Garrity <garrityk@bc.edu>
2020-04-27 00:25:42 -0400

    update save for progress with words api

---
+++ TravelWise/APIKeys.swift
@@ -0,0 +1,25 @@
+//
+//  APIKeys.swift
+//  TravelWise
+//
+//  Created by Kathleen Garrity on 4/26/20.
+//  Copyright © 2020 Kathleen Garrity. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let wordKey = "446e7fd36dmsha47214ee33f12c1p108a91jsnc04a57241e68"
+}
+
+
+/*
+ var unirest = require("unirest");
+
+ var req = unirest("GET", "https://wordsapiv1.p.rapidapi.com/");
+
+ req.headers({
+     "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+     "x-rapidapi-key": "446e7fd36dmsha47214ee33f12c1p108a91jsnc04a57241e68"
+
+ */
---
+++ TravelWise/Words.swift
@@ -0,0 +1,67 @@
+//
+//  Words.swift
+//  TravelWise
+//
+//  Created by Kathleen Garrity on 4/26/20.
+//  Copyright © 2020 Kathleen Garrity. All rights reserved.
+//
+
+import Foundation
+
+class Word: Codable {
+    
+//    struct Results: Codable {
+//        var definition = ""
+//    }
+    
+    //var results: [Results] = []
+    var definition = ""
+    
+    
+    func getData(wordToDefine: String, completed: @escaping ()->()) {
+        
+        let word = wordToDefine.replacingOccurrences(of: " ", with: "%20")
+        //let word = wordToDefine
+        
+        let url = URL(string: "https://wordsapiv1.p.mashape.com/")
+        
+        guard url != nil else {
+            print("🚫 ERROR creating URL object")
+            return
+        }
+        
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://wordsapiv1.p.rapidapi.com/\(word)")! as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
+        
+        let headers = ["x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+                       "x-rapidapi-key": "\(APIKeys.wordKey)"
+        ]
+        
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            if let error = error {
+                print("😡 ERROR: \(error.localizedDescription)")
+            }
+            if data == nil {
+                print("Data is still nil")
+            }
+            
+            
+            do {
+                let result = try JSONDecoder().decode(Word.self, from: data!)
+                self.definition = result.definition
+                
+            } catch {
+                print("😡 JSON ERROR: \(error.localizedDescription)")
+            }
+            completed()
+            
+        }
+        dataTask.resume()
+    }
+    
+}
+
